65bffdce9daa720fc111ed6849f9edf3bd6b780e
Green Bear <58751728+Josiah76@users.noreply.github.com>
2023-02-19 21:04:15 -0500

    Add files via upload

--- AntiHack.py
+++ AntiHack.py
@@ -1,4 +1,8 @@
-print("||")
-print("||")
-print("||_")
-print("|__|")
+cls
+print("||    ||")   
+print("||    ||")   
+print("||_   ||_")
+print("|__|  |__|")
+input
+print("Welcome to the LL Anti Hack Tool.")
+print("Enter Hackers Device Name:")
